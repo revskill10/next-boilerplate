4342feaca7ce1ab43338ba0ab303ce6dc2798779
Dung Truong <checkraiser11@gmail.com>
2018-09-30 07:09:06 +0700

    Add material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import App, { Container } from 'next/app'
-import { I18n as I18nR, I18nextProvider } from 'react-i18next'
+import { I18nextProvider } from 'react-i18next'
 import initialI18nInstance from '../shared/i18n'
-import LanguageSwitch from '../components/languageSwitch'
-
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getPageContext from '../shared/getPageContext';
 
 export default class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+  
   render () {
     const { Component, pageProps } = this.props
     const { i18n, initialI18nStore, initialLanguage } = pageProps || {}
@@ -17,10 +32,21 @@ export default class MyApp extends App {
           initialI18nStore={initialI18nStore}
           initialLanguage={initialLanguage}
         >
-          <React.Fragment>
-            <LanguageSwitch />
-            <Component {...pageProps} />
-          </React.Fragment>
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
+          >
+            {/* MuiThemeProvider makes the theme available down the React
+                tree thanks to React context. */}
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </MuiThemeProvider>
+          </JssProvider>
         </I18nextProvider>
       </Container>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,89 @@
-import IndexComponent from '../components';
-import '../styles/index.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
 import cookies from 'next-cookies';
 import { withI18next } from '../hocs/withI18next'
+import LanguageSwitch from '../components/languageSwitch';
 
-const Index = ({token, t}) => (
-  <>
-    <h1>{t('test')}</h1>
-    <IndexComponent token={token} />
-  </>  
-)
 
-Index.getInitialProps = async (ctx) => {
-  return cookies(ctx);
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+class Index extends React.Component {
+  static async getInitialProps(ctx) {
+    return cookies(ctx);
+  }
+
+  state = {
+    open: false,
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    const { classes, t } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>{t('test')}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="display1" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          example project
+        </Typography>
+        <Typography gutterBottom>
+          <Link href="/about">
+            <a>Go to the about page</a>
+          </Link>
+        </Typography>
+        <LanguageSwitch />
+        <Button variant="contained" color="secondary" onClick={this.handleClick}>
+          Super Secret Password
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default withI18next(['common'])(Index)
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+  token: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
+};
+
+export default withI18next(['common'])(withStyles(styles)(Index));
