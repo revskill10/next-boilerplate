8316c96d9e15d111f74c2d9232c164dd6e5b8afe
Dung Truong <checkraiser11@gmail.com>
2018-10-02 05:01:53 +0700

    Add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,22 @@ import JssProvider from 'react-jss/lib/JssProvider';
 import getPageContext from '../shared/getPageContext';
 import { ApolloProvider } from 'react-apollo'
 import withApollo from '../hocs/withApollo'
+import { PageTransition } from 'next-page-transitions'
+import Loader from '../components/loader'
+
+const TIMEOUT = 400
 
 class MyApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
   constructor(props) {
     super(props);
     this.pageContext = getPageContext();
@@ -30,28 +44,67 @@ class MyApp extends App {
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-        <I18nextProvider
-          i18n={i18n || initialI18nInstance}
-          initialI18nStore={initialI18nStore}
-          initialLanguage={initialLanguage}
-        >
-          <JssProvider
-            registry={this.pageContext.sheetsRegistry}
-            generateClassName={this.pageContext.generateClassName}
+          <I18nextProvider
+            i18n={i18n || initialI18nInstance}
+            initialI18nStore={initialI18nStore}
+            initialLanguage={initialLanguage}
           >
-            {/* MuiThemeProvider makes the theme available down the React
-                tree thanks to React context. */}
-            <MuiThemeProvider
-              theme={this.pageContext.theme}
-              sheetsManager={this.pageContext.sheetsManager}
+            <JssProvider
+              registry={this.pageContext.sheetsRegistry}
+              generateClassName={this.pageContext.generateClassName}
             >
-              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-              <CssBaseline />
-              <Component pageContext={this.pageContext} {...pageProps} />
-            </MuiThemeProvider>
-          </JssProvider>
-        </I18nextProvider>
+              {/* MuiThemeProvider makes the theme available down the React
+                  tree thanks to React context. */}
+              <MuiThemeProvider
+                theme={this.pageContext.theme}
+                sheetsManager={this.pageContext.sheetsManager}
+              >
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <PageTransition
+                  timeout={TIMEOUT}
+                  classNames='page-transition'
+                  loadingComponent={<Loader />}
+                  loadingDelay={500}
+                  loadingTimeout={{
+                    enter: TIMEOUT,
+                    exit: 0
+                  }}
+                  loadingClassNames='loading-indicator'
+                >
+                  <Component pageContext={this.pageContext} {...pageProps} />
+                </PageTransition>
+              </MuiThemeProvider>
+            </JssProvider>
+          </I18nextProvider>
         </ApolloProvider>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+            transform: translate3d(0, 20px, 0);
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+            transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity ${TIMEOUT}ms;
+          }
+          .loading-indicator-appear,
+          .loading-indicator-enter {
+            opacity: 0;
+          }
+          .loading-indicator-appear-active,
+          .loading-indicator-enter-active {
+            opacity: 1;
+            transition: opacity ${TIMEOUT}ms;
+          }
+        `}</style>
       </Container>
     )
   }
